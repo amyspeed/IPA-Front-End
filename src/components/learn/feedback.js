208086c289fac13bb30fa0802c6d3b62728809db
Amy <amy.speed.henley@gmail.com>
2019-04-05 01:02:13 -0500

    Submit scores button

--- src/components/learn/feedback.js
+++ src/components/learn/feedback.js
@@ -3,15 +3,17 @@ import React from 'react';
 export default class Feedback extends React.Component {
 
     render() {
-        const { feedback, currentPoints, points, handleClick } = this.props;
+        const { feedback, currentPoints, points, handleClick, lastQuestion } = this.props;
         return (
             <div>
                 <h3>{feedback}</h3>
                 {currentPoints ? <p>{currentPoints} Points!</p> : null}
                 {feedback && points ? <p>Your current score: {points}</p> : null}
-                {feedback ? 
+                {feedback && !lastQuestion ? 
                         <button onClick={handleClick}>Next Question</button>
                 : null}
+                {lastQuestion ? <button>Submit Score!!</button>
+                : null }
             </div>
         );
     }
