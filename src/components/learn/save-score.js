7f64ba0f04ade73583ccffbdbe06707131efe110
Amy <amy.speed.henley@gmail.com>
2019-04-09 22:10:59 -0500

    Calculations working onClick

--- src/components/learn/save-score.js
+++ src/components/learn/save-score.js
@@ -1,57 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { PutScoresByUserId, fetchScoresById } from '../../actions/scores';
+import { PutScoresByUserId } from '../../actions/scores';
 
 export class ScoreButton extends React.Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
-        this.getScores = this.getScores.bind(this);
-        this.updateNewTotal = this.updateNewTotal.bind(this);
     }
-
+    
     handleClick() {
-        
         const { finalScore, userId, currentLevel } = this.props;
-        
-        //Update current level
-        let putData = {
-            [currentLevel]: finalScore,
-        }
-        this.props.dispatch(PutScoresByUserId(userId, putData));
-    }
-
-    getScores() {
-        // Get all updated scores
-        const userId = this.props.userId;
-        this.props.dispatch(fetchScoresById(userId));
-    }
-
-    updateNewTotal() {
-        //Calculate new total
-        const userId = this.props.userId;
         const { level1, level2, level3 } = this.props.userScores;
-        const newTotal = Math.abs(level1 + level2 + level3);
+
+        //Calculate total score based on current level
+        let newTotal;
+        if (currentLevel === "level1") {
+            newTotal = Math.abs(finalScore + level2 + level3)
+            console.log(newTotal, currentLevel)
+        }
+        else if (currentLevel === "level2") {
+            newTotal = Math.abs(level1 + finalScore + level3)
+            console.log(newTotal, currentLevel)
+        }
+        else {
+            newTotal = Math.abs(level1 + level2 + finalScore)
+            console.log(newTotal, currentLevel)
+        }
+        
         const putData = {
+            [currentLevel]: finalScore,
             totalScore: newTotal
         }
-        //Update new total
+
         this.props.dispatch(PutScoresByUserId(userId, putData));
-    }
+        console.log("put current", putData);
+    };
 
     componentDidUpdate() {
     }
 
     render() {
         return(
-            <div>
-            <button onClick = {this.handleClick}>Submit Score!!
-                {/* <Link to="/" onClick = {this.handleClick}>Submit Score!!</Link> */}
+            <button>
+                <Link to="/" onClick = {this.handleClick}>Submit Score!!</Link>
             </button>
-            <button onClick = {this.getScores}>Calculate new totals</button>
-            <button><Link to="/" onClick = {this.updateNewTotal}>Back to dash!</Link></button>
-            </div>
         )
     }
 }
