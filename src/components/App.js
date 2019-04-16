416259205f7a3caad79bc27e71db9253c61563d5
Amy <amy.speed.henley@gmail.com>
2019-04-16 15:52:13 -0500

    Toastify added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {Route, withRouter} from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 import Nav from './nav-bar';
 import Landing from './landing/landing';
@@ -9,6 +10,7 @@ import Learn from './learn/learn';
 
 import { refreshAuthToken } from '../actions/auth';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 class App extends React.Component {
   componentDidUpdate(prevProps) {
@@ -37,6 +39,8 @@ class App extends React.Component {
     clearInterval(this.refreshInterval);
   }
 
+  notify = () => toast("This is toast!");
+
   render() {
     return (
       <div className="App">
@@ -44,6 +48,10 @@ class App extends React.Component {
         <Route exact path="/" component={Landing} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route path="/learn/:id" component={Learn} />
+        <div>
+          <button onClick={this.notify}>Notify !</button>
+          <ToastContainer />
+        </div>
       </div>
     );
   }
