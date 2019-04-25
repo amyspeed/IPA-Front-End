c33b6837a6de6ae927ea922f54836fd6d509eb45
Amy <amy.speed.henley@gmail.com>
2019-04-24 20:05:35 -0500

    Welcome content

--- src/components/landing/welcome.js
+++ src/components/landing/welcome.js
@@ -4,13 +4,20 @@ import React from 'react';
 export default function Welcome() {
     return (
         <div className="welcome">
-            <h1>Welcome to [aI pi eI]</h1>
-            <h2>Immersive IPA learning blah blah</h2>
+            <h1>Welcome to [ aI pi eI ]</h1>
+            <h2>This app is a gateway to your immersive International Phonetic Alphabet learning experience!</h2>
             <p>
-                Some text about what the app does. 
-                ajsklj andak ajskl akslna aksnda ajksjl.
-                To begin, create a new user accout, log in,
-                or test with a demo account. 
+                Enter here to compete with others while learning to use
+                an amazing Phonetic system utilized by linguists, 
+                speech-language pathologists, foreign-language students, 
+                translators, actors and classical singers!
+                <br /><br />
+                Have you ever wondered about those funny looking symbols next to
+                each entry in the dictionary that are supposed to help with pronunciation?
+                Leave these lessons with a greater ability to read between those brackets!
+                Soon you'll be pronouncing new foreign-language words flawlessly as well.
+                <br /><br />
+                To begin, create a new user accout or log in. 
                 Let the learning begin!
             </p>
         </div>
